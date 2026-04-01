68e14c369035b2bce4304c0089c1d0513acce828
Ryan Schill <rpschill@gmail.com>
2026-04-01 15:11:33 -0400

    Fix repo name references

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'phillies-dashboard-ng' title`, () => {
+  it(`should have the 'phillies-dashboard' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('phillies-dashboard-ng');
+    expect(app.title).toEqual('phillies-dashboard');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, phillies-dashboard-ng');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, phillies-dashboard');
   });
 });
